ae47491735ab4985c4bd0ff965dc3d7e7e9f7573
Francesco Casetta <francesco.casetta@gmail.com>
2025-08-24 09:32:18 +0200

    feat: channel refactor

--- main.go
+++ main.go
@@ -9,46 +9,52 @@ import (
 	"strings"
 )
 
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string)
+
+	go func() {
+		defer f.Close()
+		defer close(ch)
+
+		currentLine := ""
+
+		for {
+			buffer := make([]byte, 8)
+			n, err := f.Read(buffer)
+			if err != nil {
+				if currentLine != "" {
+					ch <- currentLine
+					break
+				}
+
+				if errors.Is(err, io.EOF) {
+					break
+				}
+
+				log.Fatal(err)
+			}
+
+			read := string(buffer[:n])
+			lines := strings.Split(read, "\n")
+
+			for i := 0; i < len(lines)-1; i++ {
+				ch <- fmt.Sprintf("%s%s", currentLine, lines[i])
+				currentLine = ""
+			}
+			currentLine += lines[len(lines)-1]
+		}
+	}()
+
+	return ch
+}
+
 func main() {
 	file, err := os.Open("messages.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer file.Close()
 
-	currentLine := ""
-
-	for {
-		bytes := make([]byte, 8)
-		n, err := file.Read(bytes)
-		if err != nil {
-			if currentLine != "" {
-				fmt.Printf("read: %s\n", currentLine)
-			}
-
-			if errors.Is(err, io.EOF) {
-				os.Exit(0)
-			}
-
-			log.Fatal(err)
-		}
-
-		read := string(bytes[:n])
-		lines := strings.Split(read, "\n")
-
-		if len(lines) == 1 {
-			currentLine += lines[0]
-			continue
-		}
-
-		for i, line := range lines {
-			if i != len(lines)-1 {
-				currentLine += line
-				fmt.Printf("read: %s\n", currentLine)
-				currentLine = ""
-			} else {
-				currentLine = line
-			}
-		}
+	for line := range getLinesChannel(file) {
+		fmt.Printf("read: %s\n", line)
 	}
 }
